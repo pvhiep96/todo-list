329f8ea639ef12b47a08ec8e5cae84b21d2688e8
pvhiep96 <pvhiep96@gmail.com>
2021-05-20 16:14:10 +0700

    jwt_authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   has_many :todos
+
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password,
+            length: { minimum: 6 },
+            if: -> { new_record? || !password.nil? }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_16_090413) do
+ActiveRecord::Schema.define(version: 2021_05_19_221331) do
 
   create_table "todos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.bigint "user_id"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2021_05_16_090413) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", null: false
     t.string "name"
-    t.string "password", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
